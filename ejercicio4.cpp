65676c56a67ff03d345712e84f92d7a1087fd8f0
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-02 19:03:24 -0400

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x;
+ cout<<"ingrese un numero para hacer su factorial"<<endl;
+ cin>>x;
+ cout<<":";
+ while(x>14){
+     cout<<"el numero tiene que ser menor que 14, intentelo de nuevo"<<endl;
+     cin>>x;
+ } 
+ while(x<0){
+     cout<<"no puede ser negativo, intentelo de nuevo"<<endl;
+     cin>>x;
+ }
+ 
+ int result=1;
+ for(int i=1;i<=x;++i){
+     result *=i;
+    
+ 
+ } cout<<result;
+    return 0;
+}
