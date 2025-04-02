528bd3f78bfbaf6d2319ceacd30032a0a2436750
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-02 18:36:28 -0400

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+ int response;
+ int contador=0;
+ bool haypastel= true;
+cout<<"hay torta?"<<endl;
+ while (haypastel==true and contador<10 ){
+ contador++;
+  cin>>response;
 
+ 
+ if(response==1 ){
+     cout<<"Om-nom-nom :P"<<endl;
+     
+ }else if(response==0){
+     cout<<"no hay torta"<<endl;
+     haypastel=false;
+     break;
+     
+ } else if(response==10){
+     
+ } else 
+ cout<<"su numero debe estar entre 1 y 0"<<endl;
+
+ }
+ 
+    return 0;
+}
