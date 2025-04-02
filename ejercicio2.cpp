c37ec49d66ac40db0fef27336deafb10f198c34a
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-02 17:20:28 -0400

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+ string line;
+ getline(cin,line);
+ string word="";
+ for (int i=0;i<line.size();++i){
+   if(line[i]==' '){
+    if(!word.empty()){
+    cout<<"["<<word<<"]"<<endl;
+    word="";
+    }
+    }else{
+    word+=line[i];
+    }
+ 
+ }
+  if (!word.empty()) {
+        cout << "[" << word << "]" << endl;
+    }
+    cout<<line.size()<<endl;
+ 
+    return 0;
+}
