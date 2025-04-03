a28cec3b9aee0e55b84921e7c44c46f6f956790b
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-02 21:29:44 -0400

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,1 +1,33 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   int n;
+   int k;
+   cin>>n>>k;
+   int spaces=n-1;
+   int daycont=0;
+   int day=1;
+   for (int i=0;i<spaces;++i){
+       cout<<"   ";
+       ++daycont;
+   }
+   for( day; day<=k;++day){
+       if(day<10){
+           cout<<" "<<day;
+       }else {
+       cout<< day;}
+       daycont++;
+       if(day<k){
+           cout<<" ";
+       }
+       if(daycont%7==0){
+           cout<< endl;
+       }
+      
+   }
+   
+ 
+    return 0;
+}
