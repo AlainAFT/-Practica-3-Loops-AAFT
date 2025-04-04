885976bf76474287cdc42ac3470d34ea50dd0fa3
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-04 16:32:48 -0400

    Update ejercicio14.cpp

--- ejercicio14.cpp
+++ ejercicio14.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+   string ask;
+   bool poli=0;
+   int i=0;
+   cin>>ask;
+   int e= ask.size() ;
+   for(i=0;i<ask.size();i++){
+       e--;
+       if(ask[i]==ask[e]){
+           poli=1;
+       }
+   } 
+   if(poli==1){
+       cout<<"polindormo"<<endl;
+   }
+    return 0;
+}
