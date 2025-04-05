4437612030f88e6f7959bc20c4fe059b8d078da0
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-04 22:46:22 -0400

    Update ejercicio16.cpp

--- ejercicio16.cpp
+++ ejercicio16.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int x, k;
+    cin >> x >> k;
+
+    int residuo = x;
+    int cociente = 0;
+
+    if (k == 0) {
+        cout << "Error: Division por cero." << endl;
+        return 0;
+    }
+
+    while (residuo >= k) {
+        residuo -= k;
+        cociente++;
+    }
+
+    cout << cociente << " " << residuo << endl;
+
+    return 0;
+}
